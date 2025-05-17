dcf9ee0dfb64443cd4c04752f637921696af9268
Anthony Sosa Flores <kurt.sosa@soho.cl>
2025-05-17 18:11:30 -0300

    Se agrega un mapa, despues de obener nuestras coordenadas, el mapa se ubica con animacion

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## 🚀 Funcionalidades principales
 
-- 📍 **Geolocalización en tiempo real**: muestra un mapa con la ubicación actual del usuario.
+- 📍 **Geolocalización en tiempo real**: muestra un mapa con la ubicación actual del usuario.✅
 - 📸 **Carga de imagen**: permite subir una foto del perro perdido o encontrado.
 - 🗺️ **Marcas en el mapa**:
   - Reportar perro perdido: se indica la zona donde fue visto por última vez.
@@ -18,26 +18,26 @@
 
 ## 📱 Tecnologías utilizadas
 
-- React Native (Expo)
+- React Native (Expo) SDK 53
+- Expo Location
+- MapView (react-native-maps)
 - React Navigation
 - Firebase (Auth, Firestore y Storage)
-- MapView (react-native-maps)
-- Expo Location
 - Expo ImagePicker
 
 ---
 
 ## 📱 Recursos
 
-- @expo/vector-icons - https://icons.expo.fyi/Index
+- expo-location [video](https://www.youtube.com/watch?v=ltHbdeJg9eA)
+- react-native-maps
+- @expo/vector-icons - https://icons.expo.fyi/Index (ya viene por default en el SDK)
 - expo-router [info](https://medium.com/@abdulaleemzafar515/the-benefits-of-using-expo-router-over-react-navigation-172f21772152)
 - react-native-safe-area-context
 - react-native-screens
 - expo-linking
 - expo-constants
 - expo-status-bar
-- expo-location [video](https://www.youtube.com/watch?v=ltHbdeJg9eA)
-- react-native-maps [video](https://www.youtube.com/watch?v=4EUekljBzjc)
 
 ---
 
@@ -46,3 +46,4 @@
 1. cd lostandfound
 2. npm i
 3. npx expo start
+4. Utilizo el simulador Pixel 5, Android 14, Google Apis
--- lostandfound/src/screens/LocationScreen.jsx
+++ lostandfound/src/screens/LocationScreen.jsx
@@ -1,12 +1,40 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import useLocation from "../hooks/useLocation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import MapView, { Marker } from "react-native-maps";
 
 const LocationScreen = () => {
   const { getUserLocation, latitude, longitude, errorMsg, locationDetails } =
     useLocation();
   const [hasFetched, setHasFetched] = useState(false);
+  const [mapRegion] = useState({
+    latitude: -34.603722, // Ej: Buenos Aires
+    longitude: -58.381592,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (latitude && longitude && mapRef.current) {
+      mapRef.current.animateToRegion(
+        {
+          latitude,
+          longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        },
+        1000 // duración en milisegundos
+      );
+    }
+  }, [latitude, longitude]);
 
   const handleGetLocation = () => {
     if (hasFetched) return;
@@ -20,7 +48,16 @@ const LocationScreen = () => {
         <Feather name="map-pin" size={70} color="#a1a1a1" />
       </View>
 
-      <Text style={styles.title}>Usar expo-location</Text>
+      <Text style={styles.title}>
+        {`Usar expo-location & \n`}
+        {`react-native-maps`}
+      </Text>
+
+      <MapView ref={mapRef} style={styles.map} initialRegion={mapRegion}>
+        {latitude && longitude && (
+          <Marker coordinate={{ latitude, longitude }} title="Tu ubicación" />
+        )}
+      </MapView>
 
       <Text style={styles.desc}>
         Cuando hagas click en el boton, se obtendra la ubicación de tu
@@ -115,5 +152,17 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 16,
     color: "#333",
+  },
+  // map: {
+  //   marginTop: 20,
+  //   width: Dimensions.get("window").width - 50,
+  //   height: 200,
+  //   borderRadius: 10,
+  // },
+  map: {
+    width: Dimensions.get("window").width - 50,
+    height: 300,
+    borderRadius: 10,
+    marginBottom: 10,
   },
 });
